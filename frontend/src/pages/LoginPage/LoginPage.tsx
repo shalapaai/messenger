408e1dd71a2af85bc8a3f867c36e394a06ec3fd3
SBly <sbly.03@gmail.com>
2026-06-24 18:36:16 +0300

    Add auth pages

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,16 @@
+import { LoginForm } from '../../features/auth/LoginForm'
+import { AuthLayout } from '../../widgets/AuthLayout'
+
 function LoginPage() {
-  return <h1>Login page</h1>
+  return (
+    <AuthLayout
+      isTitleWrapped
+      title="Мессенджер для команды"
+      description="Войдите в аккаунт, чтобы продолжить общение, открыть чаты и не потерять важные сообщения."
+    >
+      <LoginForm />
+    </AuthLayout>
+  )
 }
 
 export default LoginPage
